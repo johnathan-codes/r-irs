e8aa68cb720da7b235fd471f391ab1a13098d7e7
OneManDevz <42937562+OneManDevz@users.noreply.github.com>
2020-08-01 04:00:01 +0200

    reservation filter

--- src/helpers/interfaces.tsx
+++ src/helpers/interfaces.tsx
@@ -6,11 +6,13 @@ export interface RoomIF {
 	room_id: number
 	name: string
 	description: string
-	reservations: {
-		reservation_id: number
-		reservee: { name: string; user_id: number }
-		subject: string
-		start_date_iso: string
-		end_date_iso: string
-	}[]
+	reservations: ReservationIF[]
+}
+
+export interface ReservationIF {
+	reservation_id: number
+	reservee: { name: string; user_id: number }
+	subject: string
+	start_date_iso: string
+	end_date_iso: string
 }
--- src/views/RoomCard.tsx
+++ src/views/RoomCard.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
-import { RoomIF } from '../helpers/interfaces'
+import { Card, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap'
+import { RoomIF, ReservationIF } from '../helpers/interfaces'
 import '../styles/rooms.scss'
 
 const RoomCard = ({ description, reservations, room_id, name }: RoomIF) => {
+	const getCurrentReservation = (reservations: ReservationIF[]) => {
+		let currentDate = new Date()
+		let filteredReservations = reservations.filter((reservation) => {
+			return new Date(reservation.start_date_iso) > currentDate
+		})
+
+		return filteredReservations
+	}
+
+	const resSubject = (reservations: ReservationIF[]) => {
+		const currentRes = getCurrentReservation(reservations)
+		if (currentRes.length === 0) {
+			return 'Free'
+		} else {
+			return 'Subject: ' + currentRes[0].subject
+		}
+	}
+
 	return (
 		<Card className="roomCard">
 			<CardBody>
 				<CardTitle>{name}</CardTitle>
 				<CardSubtitle>{description}</CardSubtitle>
+				<hr />
+				<header>Current Reservation:</header>
+				{}
+				<CardText>{resSubject(reservations)}</CardText>
 			</CardBody>
 		</Card>
 	)
